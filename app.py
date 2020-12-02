c62b3e20ae1884482cb18f5885e2a6235cca139a
Adam Hitchcock <adam@northisup.com>
2020-12-02 10:25:40 -0800

    logging

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import os
-from northisbot.bot import NorthIsBot
+from logging import getLogger
 from pathlib import Path
 
+from northisbot.bot import NorthIsBot
+from northisbot.config import configure_logging
+
+logger = getLogger(__name__)
+
+
+configure_logging()
+
+logger.info("starting bot")
 root = Path(__file__).parent
 
-bot = NorthIsBot('!')
-bot.discover_extensions(root / 'northisbot')
+bot = NorthIsBot("!")
+bot.discover_extensions(root / "northisbot")
 bot.run(os.environ["DISCORD_NORTHISBOT_TOKEN"])
