5019f618d42af6df7c22fd680b98ae4defc1e579
Adam Hitchcock <adam@northisup.com>
2021-05-05 16:30:51 -0700

    polish the routes

--- northisbot/lib/web.py
+++ northisbot/lib/web.py
@@ -1,12 +1,15 @@
 import asyncio
+import imp
 import logging
-from functools import wraps
+from dataclasses import dataclass
+from functools import cached_property, wraps
 from typing import Callable, ClassVar, Iterable, List, Literal, Optional
 from urllib.parse import urljoin
 
 from aiohttp import web, web_middlewares
 from discord.ext import commands
 
+from northisbot.bot import NorthIsBot
 from northisbot.lib.config import AppConfig
 
 logger = logging.getLogger(__name__)
@@ -14,7 +17,10 @@ MethodsT = Literal["CONNECT", "HEAD", "GET", "DELETE", "OPTIONS", "PATCH", "POST
 
 _NO_SLASH_ERR = "may not contain a '/'"
 
+
+@dataclass
 class WebCog(commands.Cog):
+    bot: NorthIsBot
     url_root: ClassVar[Optional[str]] = None
 
     # extra middlewares to append
@@ -24,37 +30,43 @@ class WebCog(commands.Cog):
         if self.url_root is None:
             raise AttributeError(f"{self.__class__.__name__} is missing 'root' path value")
 
-        self.url_root = self.url_root.strip('/')
-        if '/' in self.url_root:
+        self.url_root = self.url_root.strip("/")
+        if "/" in self.url_root:
             raise ValueError(f"{self.__class__.__name__}.url_root {_NO_SLASH_ERR}")
 
         self.bot = bot
-        config = AppConfig(bot.__class__, self.__class__)
+        self.host = self.config.get("HOST", "0.0.0.0")
+        self.port = self.config.get("PORT", 8080)
 
-        self.host = config.get('HOST', '0.0.0.0')
-        self.port = config.get('PORT', 8080)
+    @property
+    def config(self) -> AppConfig:
+        return AppConfig(self.bot.__class__, self.__class__)
 
+    @cached_property
+    def web_app(self) -> web.Application:
         middlewares = [web.normalize_path_middleware()] + self.middlewares
-        self.app = web.Application(middlewares=middlewares)
+        app = web.Application(middlewares=middlewares)
 
         def _route_attrs() -> Iterable[Callable]:
             for name in dir(self):
-                if getattr(attr := getattr(self, name), 'is_route', False):
+                if name == "web_app":
+                    continue
+                if getattr(attr := getattr(self, name), "is_route", False):
                     yield attr
 
         for handler in _route_attrs():
-            logger.info(f'adding route: {handler.method} {handler.path}')
-            adder = getattr(self.app.router, f'add_{handler.method.lower()}')
-            adder(f'/{self.url_root}/{handler.path}/', handler)
+            path = f"/{self.url_root}/{handler.path}/"
+            logger.debug(f"[{self.__class__.__name__}] adding route: {handler.method} {path}")
+            adder = getattr(app.router, f"add_{handler.method.lower()}")
+            adder(path, handler)
 
-        logger.debug(self.app.router._resources)
-        logger.debug(self.app.router._named_resources)
-        logger.debug('server built')
+        logger.debug("web app built")
+        return app
 
     @classmethod
     def route(cls, method: MethodsT, path: str):
-        path = path.strip('/')
-        if '/' in path:
+        path = path.strip("/")
+        if "/" in path:
             raise ValueError(f"'{path}' {_NO_SLASH_ERR}")
 
         def decorator(func) -> Callable[[web.Request], web.Response]:
@@ -67,15 +79,17 @@ class WebCog(commands.Cog):
             wrapper.path = path
 
             return wrapper
+
         return decorator
 
-    @commands.Cog.listener('on_ready')
+    @commands.Cog.listener("on_ready")
     async def start(self):
-        runner = web.AppRunner(self.app)
+        runner = web.AppRunner(self.web_app)
+
         await runner.setup()
         self.site = web.TCPSite(runner, self.host, self.port)
 
-        logger.info('starting site')
+        logger.info("starting site")
         self.bot.loop.create_task(self.site.start())
 
     def __unload(self):
