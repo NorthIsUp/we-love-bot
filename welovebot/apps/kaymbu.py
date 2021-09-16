b66a9a77a44b03fdc0d45dc79e27909ac5ddc476
Adam Hitchcock <adam@northisup.com>
2021-09-16 17:10:29 -0400

    add kaymbu

--- welovebot/apps/kaymbu.py
+++ welovebot/apps/kaymbu.py
@@ -1,10 +1,14 @@
 from aiohttp.web import Request, Response
 
+from welovebot.lib.config import Config
 from welovebot.lib.web import WebCog
 
 
 class Kaymbu(WebCog):
     url_root = 'kaymbu'
+
+    class Config(Config):
+        pass
 
     @WebCog.route('GET', '/new_post')
     async def foo(self, request: Request) -> Response:
